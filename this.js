5f59d6b61fe9dffb8b225ab2255169c5d9f1a288
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-05-31 18:03:01 +0900

    this.js: 생성자 함수 호출

--- this.js
+++ this.js
@@ -72,3 +72,18 @@ const newPerson = {
 // 메서드 getName을 호출한 객체는 newPerson이다.
 console.log(newPerson.getName()); // Qbit
 // ============================================================================================================
+
+// 생성자 함수
+function Circle(radius) {
+  this.radius = radius;
+  this.getDiameter = function () {
+    return 2 * this.radius;
+  };
+}
+
+const circle1 = new Circle(5);
+const circle2 = new Circle(10);
+
+console.log(circle1.getDiameter()); // 10
+console.log(circle2.getDiameter()); // 20
+// ============================================================================================================
