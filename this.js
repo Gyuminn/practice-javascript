6573f5873f699bf5dfe64b5f867164bf760bdad0
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-05-31 17:28:04 +0900

    this가 가리키는 것들의 종류

--- this.js
+++ this.js
@@ -4,8 +4,37 @@ const circle = {
 
   // 메서드
   getDiameter() {
-    return 2 * circle.radius;
+    return 2 * this.radius;
   },
 };
 
-console.log(circle.getDiameter());
+console.log(circle.getDiameter()); // 10
+// ======================================================
+
+// 전역에서 this는 전역 객체 window를 가리킨다.
+console.log(this); // widnow
+
+// 일반 함수 내부에서 this는 전역 객체 window를 가리킨다.
+function square(number) {
+  console.log(this); // window
+  return number * number;
+}
+square(2);
+
+// 메서드 내부에서 this는 메서드를 호출한 객체를 가리킨다.
+const person = {
+  name: "Qbit",
+  getName() {
+    console.log(this); // {name: 'Qbit', getName: f}
+    return this.name;
+  },
+};
+console.log(person.getName()); // Qbit
+
+// 생성자 함수 내부에서 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+function Person(name) {
+  this.name = name;
+  console.log(this); // Person {name: 'Qbit'}
+}
+
+const me = new Person("Qbit");
