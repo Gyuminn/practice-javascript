c4c5d3d27cfc75a2c483b2683ff42f7e4c735722
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-01 16:35:10 +0900

    yarn add axios

---
+++ files/promise.js
@@ -0,0 +1,29 @@
+// Promise 객체를 반환하는 함수
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${ms} 밀리초가 지났습니다`);
+      resolve();
+    }, ms);
+  });
+};
+
+delay(1000)
+  .then(() => delay(2000))
+  .then(() => Promise.resolve("끝"))
+  .then(console.log);
+
+console.log("시작");
+// ====================================================================================================
+
+const API_URL = "https://api.github.com";
+
+// axios.get() method는 Promise 객체를 반환
+const axios = require("axios");
+// axios
+//   .get(`${API_URL}/repos/facebookincubator/create-react-app/issues?per_page=10`)
+//   .then((res) => {
+//     console.log("최근 10개의 이슈");
+//     res.data.map((issue) => issue.title).forEach((title) => console.log(title));
+//     console.log("출력이 끝났습니다");
+//   });
