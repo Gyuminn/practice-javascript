36fc412118a6a82103d6878dd21ed9bd9e2c0fb9
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-08-31 15:15:57 +0900

    feat: 타입 정의 추가가 아닌 JSDoc & allowJS 속성을 이용한 js 파일 타입 보호

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import {init, exit} from "myPackage";
+import {init, exit} from "./myPackage";
 
-
-init({
-    url:"true"
-})
-exit(1)
-// 자바스크립트 패키지를 사용하는 경우 필요한 작업
-// myPackage를 module처럼 불러오고싶다.
+// <파일 전체를 위한 타입 정의 생성 방법>
+// 자바스크립트 패키지를 사용하는 경우 필요한 작업(myPackage를 module처럼 불러오고싶다.)
 // 이를 위해서 정의 타입이 필요하고, myPackage.d.ts 파일에 타입정의를 추가시켜준다.
 // call signature까지 추가시켜주면 된다.
+// 삭제시켜놓음.
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,26 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
   return true;
 }
 
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
+
+// js를 ts 로 보호받게끔 할 수 있다.(당장 js를 ts로 변환시킬 수 없는 경우 등)
+// 1. @ts-check 추가
+// 2. tsconfig 에서 allowJS 속성 true
+// 3. .js 파일에서 type을 추가해줄 수 없지만 JSDoc을 이용해
+//    코멘트(@params)를 남김으로써 타입을 보호받도록 할 수 있다.
